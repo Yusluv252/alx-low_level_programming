b377b51be903c96c96144483859c9b394d2b64f6
yusluv252 <yusuf.olawale@yahoo.com>
2023-09-08 14:23:36 -0700

    101-qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	write(2, "that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(2, "that piece of art is useful\" - Dora Korpar, 2015-10-19", 54);
 	return (1);
 }
