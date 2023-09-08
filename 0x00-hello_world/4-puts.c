8386fa6fa011905f9d1e5737112bf92cb9bc2550
yusluv252 <yusuf.olawale@yahoo.com>
2023-09-07 17:44:25 -0700

    vi 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-prints this is the main function
+ * puzzle, follow by new line
+ * Return: always 0
+ */
+int main(void)
+
+{
+	puts('\"Programming is like building a multilingual puzzle);
+	return(0);
+}
