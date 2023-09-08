e5eaf7c7e3476fe4e10cbd0f1277ab9b3aeb7617
yusluv252 <yusuf.olawale@yahoo.com>
2023-09-08 09:28:06 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Always start the function
+ * Return:Always 0 when successful
+ */ 
+int main(void)
+{
+   printf("with proper grammar,but the outcome is a piece of art");
+   return (0)
+}
