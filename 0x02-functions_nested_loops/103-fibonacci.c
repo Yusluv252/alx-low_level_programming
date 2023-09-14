5f806b2bc7078072145783432cad88d3402ee330
yusluv252 <yusuf.olawale@yahoo.com>
2023-09-13 22:25:14 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -18,9 +18,10 @@ int main(void)
 	sum = 0;
 	for (i = 1; i <= 33; ++i)
 	{
-	if (j <= 4000000 && (j % 2) == 0);
+	if (j <= 4000000 && (j  2) == 0)
+
 		{
-			sum = sum + j;
+		sum = sum + j;
 		}
 		next = j + k;
 		j = k;
