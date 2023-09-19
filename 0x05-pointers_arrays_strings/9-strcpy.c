2e78fd671cc31b811125d015655469d4bd9eaa99
yusluv252 <yusuf.olawale@yahoo.com>
2023-09-19 11:39:31 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - function name
+ * @dest: fuction parameter 1
+ * @src: function parameter 2
+ * Return: String
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	dest[i] = src[i];
+	dest[i] = '\0';
+	}
+	return(dest);
+}
