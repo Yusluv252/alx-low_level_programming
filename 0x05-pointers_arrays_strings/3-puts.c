30b3614937a7169b506cd48dd86448cfe7e034f3
yusluv252 <yusuf.olawale@yahoo.com>
2023-09-18 21:32:10 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,15 @@
 /**
  * _puts - function that print a string
  * @str: string to be printed
- * Return: void
+ *
  */
 void _puts(char *str)
 {
-
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
+	{
 	_putchar (str[i]);
+	}
 	_putchar('\n');
 }
