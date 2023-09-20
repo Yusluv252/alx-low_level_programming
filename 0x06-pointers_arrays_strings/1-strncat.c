5f6039609c8c772b6379f27c45c84326493d9df2
yusluv252 <yusuf.olawale@yahoo.com>
2023-09-20 02:00:16 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of byte
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n && *src != '\0' ; i++)
+	{
+		dest[len = i] = src[i];
+		src++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
