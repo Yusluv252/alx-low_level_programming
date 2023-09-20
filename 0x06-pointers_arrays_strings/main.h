f8bb14adabafbc076a8bb86a48d7dc576feb1305
yusluv252 <yusuf.olawale@yahoo.com>
2023-09-20 05:53:22 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,10 +6,10 @@ int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *);
 void reverse_array(int *a, int n);
-char *string_toupper(char *str);
+char *string_toupper(char *);
 char *cap_string(char *);
-
+char *leet(char *str);
 
 #endif
