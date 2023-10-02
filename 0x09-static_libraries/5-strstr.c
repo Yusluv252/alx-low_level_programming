19bc24c71bfeafd1070c3949381847a49dcc57c0
yusluv252 <yusuf.olawale@yahoo.com>
2023-10-02 06:05:35 -0700

    ./quote

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring
+ * @haystack: the string to be search
+ * @needle: the substring to locate
+ *
+ * Return: if substring located a pointer in the begining
+ * and if not return NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+				index++;
+			} while (haystack[index] == needle[index]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
